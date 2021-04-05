41fe3d2de60d18c06734245698659b44b0829f41
coderluka <>
2021-04-05 09:21:24 +0200

    change to go run . output message in main

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 // main.go
 package main
+
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("HAllo")
-	a:=App{}
+	fmt.Println("HAllo - testing")
+	a := App{}
 	a.Initialize(
-		"postgres",//os.Getenv("APP_DB_USERNAME"),
-		"1234567890",//os.Getenv("APP_DB_PASSWORD"),
-		"cdue02")//os.Getenv("APP_DB_NAME"))
+		"postgres",   //os.Getenv("APP_DB_USERNAME"),
+		"1234567890", //os.Getenv("APP_DB_PASSWORD"),
+		"cdue02")     //os.Getenv("APP_DB_NAME"))
 	a.Run(":8010")
 }
